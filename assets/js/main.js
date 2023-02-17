5e00a7c6c7c2a2bd5657e16e33a677be7a92f203
sskkfmnn <sskkfmnn@gmail.com>
2023-02-17 14:55:05 +0100

    Lev1_3

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+console.log("");
+
+const bilder = document.getElementById("bilder");
+
+bilder.innerHTML = "<figure><img src='https://unsplash.it/200/200' alt='random img'/><figcaption>Fig.1</figcaption></figure>";
+
+bilder.innerHTML += "<figure><img src='https://unsplash.it/200/200' alt='random img'/><figcaption>Fig.2</figcaption></figure>";
+
+bilder.innerHTML += "<figure><img src='https://unsplash.it/200/200' alt='random img'/><figcaption>Fig.3</figcaption></figure>";
